c39c8bd9b81908e2108b62faa5606e7d2bd18d78
MVbys <bismaxmail@gmail.com>
2022-06-02 13:55:55 +0300

    substanse_index_view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,42 +10,89 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <link href="{{asset('css/index.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 
-<body class="antialiased">
+<body>
+    <div class="min-h-full">
+        <nav class="bg-gray-800">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex items-center justify-between h-16">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0">
+                            <img class="h-8 w-8"
+                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
+                        </div>
+                        <div class="hidden md:block">
+                            <div class="ml-10 flex items-baseline space-x-4">
+                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Substance</a>
 
-    <style>
-        .container {
-            width: 800px;
-            margin: 0 auto;
-        }
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Manufacturer</a>
 
-    </style>
-     <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    <div class="nav">
-        @php
-            $instances = ['substance'];
-            $resurces = ['index', 'create', 'show', 'edit'];
-            $id = 1;
-        @endphp
-        @foreach ($instances as $instance)
-            @foreach ($resurces as $recurce)
-                <a href="{{ route($instance.'.'.$recurce, $id) }}">{{$instance.'.'.$recurce}} </a><br>
-            @endforeach
-        @endforeach
+                                <a href="#"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Medicine</a>
 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="-mr-2 flex md:hidden">
+                        <!-- Mobile menu button -->
+                        <button type="button"
+                            class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                            aria-controls="mobile-menu" aria-expanded="false">
+                            <span class="sr-only">Open main menu</span>
+                            <!--
+                    Heroicon name: outline/menu
+
+                    Menu open: "hidden", Menu closed: "block"
+                  -->
+                            <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                            <!--
+                    Heroicon name: outline/x
+
+                    Menu open: "block", Menu closed: "hidden"
+                  -->
+                            <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+            </div>
+        </header>
+        <main>
+            <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <!-- Replace with your content -->
+                <div class="p-20 px-4 py-6 sm:px-0">
+                    <div class="border-4 border-dashed border-gray-200 rounded-lg p-20 ">
+
+                        <div class="container">
+                            @yield('content')
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /End replace -->
+            </div>
+        </main>
     </div>
 
-    <div class="container">
-        @yield('content')
-    </div>
-
-
-{{-- @include('test') --}}
 
 
 </body>
--- resources/views/substance/index.blade.php
+++ resources/views/substance/index.blade.php
@@ -1,12 +1,48 @@
 @extends('index')
 
 @section('content')
-    <h1>Substances</h1>
-    @foreach ($substances as $substance)
-        {{ $substance->id }} -
-        {{ $substance->title }} <br />
-        <hr>
-    @endforeach
+    <form method="POST" class="mt-1 mb-2 flex items-center">
+        @csrf
+        <label for="title" class="w-1/5 mx-2 block text-base font-bold text-gray-700">Substance title</label>
+
+        <input type="text" name="title" id="title" autocomplete="given-name"
+            class="mx-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+        <button type="submit"
+            class="mx-2 w-1/4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Add
+        </button>
+    </form>
+
+
+
+    <table class="border-collapse border border-slate-300 w-full mb-2 text-center ...">
+        <thead>
+            <tr>
+                <th class="border border-slate-500 w-20 ...">Id</th>
+                <th class="border border-slate-500 ...">Substance</th>
+                <th class="border border-slate-500 ...">Options</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($substances as $substance)
+                <tr>
+                    <td class="border  ...">{{ $substance->id }}</td>
+                    <td class="border  ...">{{ $substance->title }}</td>
+                    <td class="border  ...">
+                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button type="submit"
+                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-amber-500 hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500">Show</button>
+                            <button type="submit"
+                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-500 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">Update</button>
+                            <button type="submit"
+                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">Delete</button>
+
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
     {{ $substances->links('vendor.pagination.tailwind') }}
 @endsection
